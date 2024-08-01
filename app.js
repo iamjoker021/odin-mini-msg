054456bc2e982994d266c41acbc5c27743b2917e
iamjoker021 <iamjoker021@gmail.com>
2024-08-01 14:41:31 +0530

    feat: Add Index Page and loop messages

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
+const path = require("node:path");
 const express = require('express');
+const { title } = require("node:process");
 const app = express();
 
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ];
+
 app.get('/', (req, res) => {
-    res.send('Hello, World')
+    res.render('index', { title: "Mini Messageboard", messages: messages })
 })
 
 const PORT = 3000
